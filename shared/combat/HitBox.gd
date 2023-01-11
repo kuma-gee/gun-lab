3ae30943bc7447ef3b3dd24045d8d0628e9c1392
Min Yin <minyin142p@gmail.com>
2023-01-11 15:42:16 +0100

    add gun line

--- shared/combat/HitBox.gd
+++ shared/combat/HitBox.gd
@@ -3,7 +3,6 @@ extends Area2D
 class_name HitBox
 
 export var damage = 1
-export var free_on_hit = true
 
 func _ready():
 	connect("area_entered", self, "_on_HitBox_area_entered")
@@ -11,4 +10,3 @@ func _ready():
 func _on_HitBox_area_entered(area):
 	if area is HurtBox:
 		area.damage(damage)
-		queue_free()
