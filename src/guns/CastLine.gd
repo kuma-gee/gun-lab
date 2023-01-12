742410905d58d967034768ba7882c5a20a358ed1
Min Yin <minyin142p@gmail.com>
2023-01-12 09:20:25 +0100

    add firerate

--- src/guns/CastLine.gd
+++
@@ -1,6 +0,0 @@
-extends Line2D
-
-onready var ray_cast = get_parent()
-
-func _process(_delta):
-	points = [to_local(ray_cast.global_position), to_local(ray_cast.get_collision_point())]
--- src/guns/Pistol.gd
+++ src/guns/Pistol.gd
@@ -3,9 +3,19 @@ extends Node2D
 export var bullet_scene: PackedScene
 
 onready var bullet_pos := $Position2D
+onready var fire_rate_timer := $FireRate
+
+var can_fire = true
 
 func fire():
+	if not can_fire: return
+	
 	var bullet = bullet_scene.instance()
 	get_tree().current_scene.add_child(bullet)
 	bullet.global_position = bullet_pos.global_position
 	bullet.global_rotation = bullet_pos.global_rotation
+	can_fire = false
+	fire_rate_timer.start()
+
+func _on_FireRate_timeout():
+	can_fire = true
