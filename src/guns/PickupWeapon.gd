3a6f1c01251825c2918e09243c3875943756bea6
myin <minyin142p@gmail.com>
2023-01-13 21:31:26 +0100

    add some sounds

--- src/guns/PickupWeapon.gd
+++ src/guns/PickupWeapon.gd
@@ -1,7 +1,14 @@
 extends Interactable
 
 export var weapon_scene: PackedScene
+onready var audio := $AudioStreamPlayer
 
 func interact(actor: Player):
+	hide()
+	
 	actor.pickup_weapon(weapon_scene)
+	audio.play()
+
+
+func _on_AudioStreamPlayer_finished():
 	queue_free()
--- src/map/Room.gd
+++ src/map/Room.gd
@@ -1,6 +1,12 @@
 class_name ParentRoom extends TileMap
 
 onready var room := $Room
+onready var bgm := $BGM
 
 func remove_tile(coor: Vector2):
 	room.set_cellv(coor, -1)
+
+
+func _on_BGM_finished():
+	if not bgm.playing:
+		bgm.play()
