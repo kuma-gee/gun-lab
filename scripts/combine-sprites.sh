bdb26f37e8ea587f3fffee4f6ff12fb8003ab457
myin <minyin142p@gmail.com>
2023-01-13 17:21:38 +0100

    add sprites differently

---
+++ scripts/combine-sprites.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python ./scripts/anim_combine.py -d ./assets/_sprites -o ./assets
--- src/map/Button.gd
+++ src/map/Button.gd
@@ -2,5 +2,8 @@ extends Node2D
 
 signal hit()
 
+onready var sprite := $Sprite
+
 func _on_HurtBox_hit():
 	emit_signal("hit")
+	sprite.frame = 1
